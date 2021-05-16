1b3720bd6c243b6e832b51fa8c1677a9ec33add0
boheliangjie <2738707744@qq.com>
2021-05-16 15:39:38 +0800

    Site updated: 2021-05-16 15:39:23

---
+++ js/ClickShowText.js
@@ -0,0 +1,38 @@
+作者：Jack Zhao
+链接：https://zhuanlan.zhihu.com/p/112229976
+来源：知乎
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#FF0000"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+      3000,
+      function() {
+          $i.remove();
+      });
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
